9509e55b3e26da6bff2c65f16a8c9868e4fdf195
ram211222 <kakarapartiabhiram@gmail.com>
2021-06-28 18:21:48 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+	public static void main(String args[]){
+		int a=5,b=10;
+		int c=a+b;
+		System.out.println("number is "+ c)
+	}
+}
---
+++ SecondSample.java
@@ -0,0 +1,7 @@
+public class SecondSample{
+	public static void main(String args[]){
+		float a=5,b=10;
+		float c=a+b;
+		System.out.println("number is "+ c)
+	}
+}
