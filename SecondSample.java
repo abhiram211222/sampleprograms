db39efe39f3302a3c3c6c73483df3afd1c4a8e1a
abhiram211222 <86592171+abhiram211222@users.noreply.github.com>
2021-07-09 19:09:39 +0530

    Update SecondSample.java

--- SecondSample.java
+++ SecondSample.java
@@ -2,6 +2,6 @@ public class SecondSample{
 	public static void main(String args[]){
 		float a=5,b=10;
 		float c=a+b;
-		System.out.println("number is "+ c)
+		System.out.println("number is "+ c);
 	}
 }
